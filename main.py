59b79fd3fec852fe59faa6dd32a9ab6fcb1b8b64
BEASTBOYJAY <jay31sinha@gmail.com>
2025-04-28 00:58:28 +0530

    Added memory to the agent

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ from source.llm import llm_model
 from source.tools import Tools
 from langchain.agents import AgentExecutor, create_react_agent
 from langchain_core.prompts import PromptTemplate
+from langchain.memory import ConversationBufferMemory
 
 
 class Agent:
     def __init__(self):
         self.llm = llm_model().model()
         self.tools = Tools().run()
-        template = """Answer the following questions as best you can. You have access to the following tools:
+        self.memory = ConversationBufferMemory(
+            memory_key="chat_history", return_messages=True
+        )
+
+        template = """Answer the following questions as best you can. 
+        You have access to the following tools:
 
         {tools}
+
+        Previous conversation history:
+        {chat_history}
 
         Use the following format:
 
@@ -39,6 +48,7 @@ class Agent:
             verbose=True,
             max_iterations=3,
             handle_parsing_errors=True,
+            memory=self.memory,
         )
 
         while True:
@@ -48,7 +58,7 @@ class Agent:
                 break
             try:
                 result = agent_executor.invoke({"input": user_input})
-                print(f"\nAgent Response:\n{result["output"]}")
+                print(f"\nAgent Response:\n{result['output']}")
             except Exception as e:
                 print(f"\n⚠️ An error occurred: {e}")
 
