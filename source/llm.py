020fdedfc6c336912cee1378341646b5d5aaf5c4
BEASTBOYJAY <jay31sinha@gmail.com>
2025-04-28 22:39:58 +0530

    Groq llm added

--- source/llm.py
+++ source/llm.py
@@ -1,13 +1,14 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
 
 
 class llm_model:
     def __init__(self):
-        self.llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+        self.gemini_llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+        self.groq_llm = ChatGroq(model="deepseek-r1-distill-llama-70b")
 
-    def run(self, query: str):
-        result = self.llm.invoke(query)
-        return result.content
+    def gemini_llm_model(self):
+        return self.gemini_llm
 
-    def model(self):
-        return self.llm
+    def groq_llm_model(self):
+        return self.groq_llm
